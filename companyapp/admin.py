32845e485dfabc2c0b501af09eb338e064e08d54
20005049 <harishmohan0701@gmail.com>
2021-01-22 15:36:28 +0000

    comments

---
+++ companyapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import People,Peoplelist,Product,Productlist
+
+# Register your models here.
+admin.site.register(People,Peoplelist)
+admin.site.register(Product,Productlist)
+
